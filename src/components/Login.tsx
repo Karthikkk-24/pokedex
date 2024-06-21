e3f04c359d3cec50b0e918c9927248520569e5e5
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-21 08:38:39 +0530

    Added framermotion and other components from magic ui

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import BaseURL from "./BaseURL";
+import ShineBorder from "./ShineBox";
+import ShinyButton from "./ShinyButton";
 
 export default function Login() {
 
@@ -33,8 +35,8 @@ export default function Login() {
     }
 
     return (
-        <div className="w-screen h-screen bg-primary flex justify-center items-center overflow-hidden bg-background">
-            <div className="h-auto w-[30rem] flex flex-col items-center gap-6 justify-center shadow-xl rounded-2xl p-7">
+        <div className="w-screen h-screen bg-slate-100 flex justify-center items-center overflow-hidden bg-background">
+            <div className="h-auto border-2 w-[30rem] flex flex-col items-center gap-6 justify-center shadow-xl rounded-2xl p-7">
                 <h2 className="text-2xl font-semibold text-center uppercase">Login</h2>
                 <div className="flex flex-col gap-2 items-start justify-start w-full h-auto">
                     <label htmlFor="" className="font-semibold text-left text-sm">
@@ -50,6 +52,7 @@ export default function Login() {
                 </div>
                 <div className="flex w-full h-auto flex-col items-center justify-center gap-5">
                     <button onClick={handleSubmit} className="bg-primary h-auto w-auto uppercase px-5 py-3 text-white rounded-lg font-semibold hover:scale-110 transition-all">Login</button>
+                    <ShinyButton />
                     <Link to="/register">Don`t have an account? Sign Up</Link>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,20 @@ export default {
                 primary: '#e94848',
                 secondary: '#4d4c4c',
                 tertiary: '#1eb3c8'
-            }
+            },
+            keyframes: {
+                "shine-pulse": {
+                    "0%": {
+                        "background-position": "0% 0%",
+                    },
+                    "50%": {
+                        "background-position": "100% 100%",
+                    },
+                    to: {
+                        "background-position": "0% 0%",
+                    },
+                },
+            },
         },
     },
     plugins: [],
