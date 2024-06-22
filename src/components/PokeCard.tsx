29edf5d85d1d1e1e4da1f9a6104bfa2610ba104f
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-23 01:16:07 +0530

    Added more pokemons to the object and fixed type conversion error

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -3,20 +3,23 @@ import PropTypes from 'prop-types';
 export default function PokeCard({
     name,
     location,
+    number,
 }: {
     name: string;
     location: string;
+    number: number;
 }) {
     return (
-        <div className='flex flex-col items-center justify-center gap-5'>
-            <div className="h-52 w-52 aspect-square rounded-xl overflow-hidden relative p-6 border-2 border-slate-100 bg-green-100">
-                <img
-                    src={location} alt={name}
-                />
+        <div className="flex flex-col items-center justify-center gap-5">
+            <div className="h-52 w-52 aspect-square rounded-xl overflow-hidden relative p-6 border-2 border-slate-50 bg-green-50">
+                <img src={location} alt={name} />
             </div>
-            <h4 className="font-semibold text-xl uppercase">
-                {name}
-            </h4>
+            <div className='flex flex-col items-start justify-start gap-1 w-full'>
+                <p className="m-0 text-left w-full">{number}</p>
+                <h4 className="font-semibold text-left w-full m-0 text-xl uppercase">
+                    {name}
+                </h4>
+            </div>
         </div>
     );
 }
@@ -24,4 +27,5 @@ export default function PokeCard({
 PokeCard.propTypes = {
     name: PropTypes.string,
     location: PropTypes.string,
+    number: PropTypes.number,
 };
