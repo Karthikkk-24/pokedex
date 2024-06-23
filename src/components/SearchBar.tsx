17bde1d900e5153974de9560c562ae7dc4d4d1da
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-23 15:08:07 +0530

    Added PokeList Component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,36 @@
 export default function SearchBar() {
     return (
         <div className="w-[60%] h-auto flex items-center justify-center border-2 border-slate-200 rounded-2xl">
-            <button type="button" className="w-16 h-full flex items-center justify-center cursor-pointer">
+            <button
+                type="button"
+                className="w-16 h-full flex items-center justify-center cursor-pointer"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="lightgray"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="icon icon-tabler icons-tabler-outline icon-tabler-search"
+                >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                    <path d="M21 21l-6 -6" />
+                </svg>
+            </button>
+            <input
+                type="text"
+                placeholder="Enter Number or Name"
+                className="w-full pl-3 h-12 rounded-xl active:border-none focus-within:outline-none"
+            />
+            <button
+                type="button"
+                className="w-16 h-full flex items-center justify-center cursor-pointer"
+            >
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -26,11 +55,6 @@ export default function SearchBar() {
                     <path d="M19 18l1 0" />
                 </svg>
             </button>
-            <input
-                type="text"
-                placeholder="Enter Number or Name"
-                className="w-full pl-3 h-12 rounded-xl"
-            />
         </div>
     );
 }
