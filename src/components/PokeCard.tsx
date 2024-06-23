27b645e2804e99e19dd3d0b7a31066daa4dc8420
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-23 14:59:42 +0530

    Matching Colour Cards Done

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -43,6 +43,50 @@ const getTypeColor = (type : string) => {
     }
 };
 
+
+const getBackgroundColor = (type : string) => {
+    switch (type.toLowerCase()) {
+        case 'normal':
+            return 'bg-gray-50';
+        case 'fire':
+            return 'bg-red-50';
+        case 'water':
+            return 'bg-blue-50';
+        case 'electric':
+            return 'bg-yellow-50';
+        case 'grass':
+            return 'bg-green-50';
+        case 'ice':
+            return 'bg-cyan-50';
+        case 'fighting':
+            return 'bg-orange-50';
+        case 'poison':
+            return 'bg-purple-50';
+        case 'ground':
+            return 'bg-amber-50';
+        case 'flying':
+            return 'bg-indigo-50';
+        case 'psychic':
+            return 'bg-pink-50';
+        case 'bug':
+            return 'bg-lime-50';
+        case 'rock':
+            return 'bg-stone-50';
+        case 'ghost':
+            return 'bg-violet-50';
+        case 'dragon':
+            return 'bg-teal-50';
+        case 'dark':
+            return 'bg-neutral-50';
+        case 'steel':
+            return 'bg-slate-50';
+        case 'fairy':
+            return 'bg-rose-50';
+        default:
+            return 'bg-gray-50';
+    }
+};
+
 export default function PokeCard({
     name,
     location,
@@ -56,7 +100,7 @@ export default function PokeCard({
 }) {
     return (
         <div className="flex flex-col items-center justify-center gap-5 cursor-pointer hover:scale-105 transition-all">
-            <div className={`h-52 w-52 aspect-square rounded-xl overflow-hidden relative p-6 border-2 border-slate-50 bg-green-50`}>
+            <div className={`h-52 w-52 aspect-square rounded-xl overflow-hidden relative p-6 border-2 border-slate-50 ${getBackgroundColor(types[0])}`}>
                 <img src={location} alt={name} />
             </div>
             <div className="flex flex-col items-start justify-start gap-1 w-full relative">
