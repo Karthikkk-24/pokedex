b664338bf203dfcfbcec6724dd60ce0cbdc643e5
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-25 23:49:22 +0530

    added raw layout for pokemon page

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -5,8 +5,21 @@ const Pokemon: React.FC = () => {
     const { name } = useParams<{ name: string }>();
 
     return (
-        <div className="h-[80vh] w-full rounded-tl-3xl rounded-tr-3xl bg-white relative z-50">
-            <h1>Pokemon: {name}</h1>
+        <div className="h-screen w-screen flex items-center justify-center flex-col">
+            <div className="w-[80%] h-full flex items-center justify-center gap-10">
+                <div className="w-1/2 h-full flex flex-col items-center justify-center">
+                    <div className="w-96 h-auto">
+                        <img
+                            src={`/public/pokemons/${name}.png`}
+                            className="w-96 h-auto"
+                            alt=""
+                        />
+                    </div>
+                </div>
+                <div className="w-1/2 h-full flex flex-col items-start justify-center">
+                    sjdfioushjifopjsopfjpsjfpo
+                </div>
+            </div>
         </div>
     );
 };
